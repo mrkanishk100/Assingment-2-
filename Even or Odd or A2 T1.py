e747f39dcc679bc087e3e7067b3c6d9e5f12621e
mrkanishk100 <mrkanishkrajput82@gmail.com>
2025-11-02 01:09:51 +0530

    Add files via upload

---
+++ Even or Odd or A2 T1.py
@@ -0,0 +1,7 @@
+num = int(input('Enter Number: '))
+
+if num % 2 == 1:
+    print(f'{num} is an odd number.')
+
+else:
+    print(f'{num} is an even number.')
---
+++ sum of no. or A2 T2.py
@@ -0,0 +1,7 @@
+a = []
+
+for i in range(1,51):
+    a.append(i)
+
+b = sum(a)
+print(f'The sum of numbers from 1 to 50 is: {b}')
