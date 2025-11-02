6d3ff564033b5ec85f76d3b97379b7e007ad3897
mrkanishk100 <mrkanishkrajput82@gmail.com>
2025-11-02 15:43:35 +0530

    Update sum of no. or A2 T2.py
    I forget to add comments.

--- sum of no. or A2 T2.py
+++ sum of no. or A2 T2.py
@@ -1,7 +1,9 @@
-a = []
+a = []                #Taking a empty list to store elements.
 
-for i in range(1,51):
-    a.append(i)
+for i in range(1,51): #Making 'for' loop for counting of 1 to 50.
+    a.append(i)       #Adding counting 1 to 50 one by one in empty list 'a'.
 
-b = sum(a)
+b = sum(a)            #Summation of elements which stored in list 'a'.
+
 print(f'The sum of numbers from 1 to 50 is: {b}')
+
