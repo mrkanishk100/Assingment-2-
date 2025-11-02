b612ffd2b5d99546e028ad5e68b692ac7bc28b30
mrkanishk100 <mrkanishkrajput82@gmail.com>
2025-11-02 15:55:37 +0530

    Update Even or Odd or A2 T1.py
    I forgot to add comments.

--- Even or Odd or A2 T1.py
+++ Even or Odd or A2 T1.py
@@ -1,7 +1,8 @@
-num = int(input('Enter Number: '))
+num = int(input('Enter Number: '))          # Taking User Input.
 
-if num % 2 == 1:
-    print(f'{num} is an odd number.')
+if num % 2 == 1:                            # Dividing user input by 2 if reminder is 0 it will print 'Odd'.
+    print(f'{num} is an odd number.')       
 
-else:
+else:                                       # Otherwise it will print 'Even'.
+
     print(f'{num} is an even number.')
